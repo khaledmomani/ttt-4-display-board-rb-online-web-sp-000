8d754eb72c57550d5c80235832a792a4e4f27d2e
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 20:35:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,12 +9,3 @@ end
 
 display_board
 
-def display_board_center_X (board= [" ","X"," "," "," "," "," "," "," "])
-   puts " #{board[0]} |#{board[1]}| #{board[2] } "
-  puts "-----------"
-  puts " #{board[3]} |#{board[4]}| #{board[5] } "
-  puts "-----------"
-  puts " #{board[6]} |#{board[7]}| #{board[8] } "
-end
-
-display_board_center_X
